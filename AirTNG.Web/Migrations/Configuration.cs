b0a09ce7aba4ffda69e9390d66c8cedfa23f08eb
Agustin Camino <agustin.camino@gmail.com>
2015-10-28 14:16:39 -0500

    This is where it all begins

---
+++ AirTNG.Web/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+using System.Data.Entity.Migrations;
+using AirTNG.Web.Models;
+
+namespace AirTNG.Web.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# AirTNG App: Part 1 - Workflow Automation with Twilio - ASP.NET MVC
+[![Build status](https://ci.appveyor.com/api/projects/status/t8vnms8v35y1mul4?svg=true)](https://ci.appveyor.com/project/TwilioDevEd/airtng-csharp)
+
+Learn how to automate your workflow using Twilio's REST API and Twilio SMS. This example app is a vacation rental site, where the host can confirm a reservation via SMS.
