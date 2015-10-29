f3b7329c9f5d2a14fecf08fc9cb206a69b8ca3d8
Agustin Camino <agustin.camino@gmail.com>
2015-10-29 08:28:42 -0500

    Remove remember me from Login

--- AirTNG.Web/Controllers/AccountController.cs
+++ AirTNG.Web/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace AirTNG.Web.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -80,7 +80,7 @@ namespace AirTNG.Web.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = false });
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
