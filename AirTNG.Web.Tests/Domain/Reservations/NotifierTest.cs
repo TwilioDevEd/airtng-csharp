3713207fb735c48c15663e7b5aa631e875bf875f
Hector Ortega <hector.oj@gmail.com>
2016-12-29 12:10:06 -0800

    Fix broken tests

--- AirTNG.Web.Tests/Domain/Reservations/NotifierTest.cs
+++ AirTNG.Web.Tests/Domain/Reservations/NotifierTest.cs
@@ -7,30 +7,22 @@ using NUnit.Framework;
 using Twilio;
 using Twilio.Clients;
 using Twilio.Http;
+using System;
 
 namespace AirTNG.Web.Tests.Domain.Reservations
 {
     public class NotifierTest
     {
         [Test]
-        public async void WhenThereAreMoreThanOneReservation_ThenAnyMessageIsSent()
+        public async void WhenThereAreMoreThanOneReservations_ThenNoMessageIsSent()
         {
             // Given
-            TwilioClient.Init("AccountSid", "AuthToken");
-            var mockClient = new Mock<TwilioRestClient>(null, null);
-            mockClient
-                .Setup(c => c.Request(It.IsAny<Request>()));
-            TwilioClient.SetRestClient(mockClient.Object);
-            var mockRepository = new Mock<IReservationsRepository>();
-            mockRepository
-                .Setup(r => r.FindPendingReservationsAsync())
-                .ReturnsAsync(new List<Reservation>
-                {
+            var mockClient = new Mock<ITwilioRestClient>();
+            var mockRepository = SetupRepositoryMock(new List<Reservation>() {
                     new Reservation(),
                     new Reservation()
                 });
-
-            var notifier = new Notifier(mockRepository.Object);
+            var notifier = BuildNotifier(mockClient, mockRepository);
 
             // When
             await notifier.SendNotificationAsync(new Reservation());
@@ -40,21 +32,13 @@ namespace AirTNG.Web.Tests.Domain.Reservations
         }
 
         [Test]
-        public async void WhenThereAreLessThanOneReservation_ThenAMessageIsSent()
+        public async void WhenThereIsOneOrNoReservation_ThenAMessageIsSent()
         {
             // Given
-            TwilioClient.Init("AccountSid", "AuthToken");
-            var mockClient = new Mock<TwilioRestClient>(null, null);
-            mockClient
-                .Setup(c => c.Request(It.IsAny<Request>()));
-            var mockRepository = new Mock<IReservationsRepository>();
-            mockRepository
-                .Setup(r => r.FindPendingReservationsAsync())
-                .ReturnsAsync(new List<Reservation>());
-
-            var notifier = new Notifier(mockRepository.Object);
-
             const string hostPhoneNumber = "host-phone-number";
+            var mockClient = SetupTwilioRestClientMock();
+            var mockRepository = SetupRepositoryMock(new List<Reservation>());
+            var notifier = BuildNotifier(mockClient, mockRepository);
 
             // When
             await notifier.SendNotificationAsync(new Reservation
@@ -65,6 +49,35 @@ namespace AirTNG.Web.Tests.Domain.Reservations
 
             // Then
             mockClient.Verify(c => c.Request(It.IsAny<Request>()), Times.Once);
+        }
+
+        private static Notifier BuildNotifier(Mock<ITwilioRestClient> mockClient, Mock<IReservationsRepository> mockRepository)
+        {
+            return new Notifier(mockRepository.Object,
+                            mockClient.Object,
+                            "AccountSid",
+                            "AuthToken");
+        }
+
+        private static Mock<IReservationsRepository> SetupRepositoryMock(List<Reservation> reservations)
+        {
+            var mockRepository = new Mock<IReservationsRepository>();
+            mockRepository
+                .Setup(r => r.FindPendingReservationsAsync())
+                .ReturnsAsync(reservations);
+            return mockRepository;
+        }
+
+        private static Mock<ITwilioRestClient> SetupTwilioRestClientMock()
+        {
+            var mockClient = new Mock<ITwilioRestClient>();
+            mockClient
+                .Setup(c => c.Request(It.Is<Request>(
+                    r => r.Method == HttpMethod.Post &&
+                    r.ConstructUrl().AbsoluteUri.Contains("Messages.json"))
+                    ))
+                .Returns(new Response(System.Net.HttpStatusCode.OK, ""));
+            return mockClient;
         }
     }
 }
